6e9cc6556ca038c2fd47122ac4652c5ccd99b177
Illia <kroshka.illya@lll.kpi.ua>
2024-03-13 15:37:23 +0200

    feature/implement template repository

---
+++ src/database/repositories/TemplateRepository.js
@@ -0,0 +1,38 @@
+'use strict'
+
+require('dotenv').config({ path: './config/.env' });
+const { Database } = require('metasql')
+
+const configJSON = process.env.NEW_DATABASE_CONFIG_JSON;
+const config = JSON.parse(configJSON);
+
+const database = new Database(config);
+class TemplateRepository {
+  constructor(tableName, db = database) {
+    this.tableName = tableName;
+    this.db = db
+  }
+  async select (where, fields = ['*']) {
+    return this.db.select(this.tableName, fields, where);
+  }
+
+  async update(data, where) {
+    return this.db.update(this.tableName, data, where)
+  }
+
+  async create(data) {
+    return this.db.insert(this.tableName, data);
+  }
+
+  async remove(data) {
+    return this.db.delete(this.tableName, data);
+  }
+}
+
+const usersRepository = new TemplateRepository('users');
+const premiumUsersRepository = new TemplateRepository('premium_users');
+
+module.exports = {
+  usersRepository,
+  premiumUsersRepository,
+}
